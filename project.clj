2ed54543a97ec98d7d6cc3792ee5564ce5b6a162
Matthew Downey <matthewdowney20@gmail.com>
2021-01-22 15:08:11 -0600

    Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.sixtant/rfc6979 "0.1.0-SNAPSHOT"
+(defproject io.sixtant/rfc6979 "0.1.0"
   :description "RFC 6979's deterministic DSA/ECDSA signature scheme in Clojure for arbitrary elliptic curves."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
